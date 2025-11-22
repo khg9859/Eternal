6f6b0670cf6f2641d1ff66189d0bc5ad96fa1b0d
범창 <45908859+ggaa1@users.noreply.github.com>
2025-11-22 17:23:41 +0900

    update

--- Desktop/Test_Eter/Eternal_FE/Eternal/src/pages/ResultsPage.js
+++ Desktop/Test_Eter/Eternal_FE/Eternal/src/pages/ResultsPage.js
@@ -66,7 +66,7 @@ export default function ResultsPage() {
     const fetchRagData = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch('http://localhost:8000/rag/search', {
+        const response = await fetch('http://54.252.198.188:8000/rag/search', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
